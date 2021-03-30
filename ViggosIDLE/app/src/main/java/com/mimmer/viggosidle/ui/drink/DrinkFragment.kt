ef6741ce616f0c0ee1e4b55e12d4241eda540e24
xmimmer <madsmoellerh4nsen@hotmail.com>
2021-03-30 15:18:05 +0200

    Added counter
    Temporary, as the counter is not yet MutableLiveData.

--- ViggosIDLE/app/src/main/java/com/mimmer/viggosidle/ui/drink/DrinkFragment.kt
+++ ViggosIDLE/app/src/main/java/com/mimmer/viggosidle/ui/drink/DrinkFragment.kt
@@ -1,12 +1,13 @@
 package com.mimmer.viggosidle.ui.drink
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mimmer.viggosidle.R
 
@@ -23,9 +24,20 @@ class DrinkFragment : Fragment() {
                 ViewModelProviders.of(this).get(DrinkViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val score: TextView = root.findViewById(R.id.score)
+        val imageView: ImageView = root.findViewById(R.id.main_image)
+
+        imageView.setOnClickListener() {
+            Log.i("ClickTag","Main image clicked!")
+            homeViewModel.currentScore += 1
+            homeViewModel.score.value
+
+            Log.i("CurrentScore", homeViewModel.currentScore.toString())
+            Log.i("score value", homeViewModel.score.value.toString())
+
+            score.text = homeViewModel.currentScore.toString()
+
+        }
         return root
     }
 }
--- ViggosIDLE/app/src/main/java/com/mimmer/viggosidle/ui/drink/DrinkViewModel.kt
+++ ViggosIDLE/app/src/main/java/com/mimmer/viggosidle/ui/drink/DrinkViewModel.kt
@@ -6,8 +6,23 @@ import androidx.lifecycle.ViewModel
 
 class DrinkViewModel : ViewModel() {
 
+    var currentScore = 0
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is the drink Fragment"
     }
+
+    private var _score = MutableLiveData<Int>().apply {
+        value = currentScore
+    }
+
     val text: LiveData<String> = _text
+    val score: LiveData<Int> = _score
+
+
+
 }
+
+
+
+
